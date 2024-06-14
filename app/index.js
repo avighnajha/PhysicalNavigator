a2483d4970e776e80c9a7a63e24ccc402fb251e6
Avighna Jha <avighnajha1703@gmail.com>
2024-06-14 11:28:24 +0100

    Basic buttons and start journey pop up

---
+++ app/index.js
@@ -0,0 +1,68 @@
+import { useRouteInfo, useRouter } from 'expo-router/build/hooks';
+import {useState} from 'react';
+import {View, ScrollView, SafeAreaView, Text, TouchableOpacity, Image, Modal} from 'react-native';
+import {Stack} from 'expo-router';
+import styles from '../styles.style';
+import {COLORS, icons, images, SIZES} from "../constants";
+
+
+const Home =()=>{
+    const router = useRouter();
+    const [modalVisible, setModalVisible] = useState(false);
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor:COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: {
+                        backgroundColor: COLORS.lightWhite,
+                    },
+                    headerShadowVisible: false,
+                    headerRight: () => (
+                        <TouchableOpacity style={styles.btnContainerHead} onPress = {()=>{setModalVisible(true)}}>
+                            <Image source={icons.chevronLeft} resizeMode="cover"
+                            style={styles.btnImg("100%")}
+                            onError={(error) => console.log(error)}
+                            />
+                        </TouchableOpacity>
+                    ),
+                    headerTitle: ""
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator={false} style={{flex:1}}>
+                <View style={{flex:0,padding: SIZES.xSmall, alignItems: "center", justifyContent: "center"}}>
+                    <TouchableOpacity 
+                    style={styles.btnContainer}
+                    onPress={() => {setModalVisible(true)}}
+                    >
+                        <Text style={{fontSize: 20}}>Start Journey</Text>
+                    </TouchableOpacity>
+                    <Modal 
+                        animationType="none"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={()=>{setModalVisible(!modalVisible)}}
+                        >
+                            <View style={{flex:0.25, padding: SIZES.medium, justifyContent: "center", alignItems: "center"}}>
+                                <TouchableOpacity style={styles.btn2}>
+                                    <Text style={{fontSize: 20}}>Use Current Location</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.btn2}>
+                                    <Text style={{fontSize: 20}}>Select location</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.btn2} onPress={() => setModalVisible(false)}>
+                                <Text style={{fontSize: 20}}>Close</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </Modal>
+                </View>
+                <View style={{flex:1, padding: SIZES.xLarge, justifyContent: "center", alignItems: "center"}}>
+                    <TouchableOpacity style={styles.btnContainer}>
+                        <Text style={{fontSize: 20}}>End Journey</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Home;
---
+++ styles.style.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+import { COLORS, FONT, SHADOWS, SIZES } from "./constants";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+    },
+    btnContainerHead: {
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.gray,
+        borderRadius: SIZES.small / 1.25,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    btnContainer: {
+        width: 200,
+        height: 100,
+        backgroundColor: COLORS.gray2,
+        borderRadius: SIZES.small / 1.25,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    btn2: {
+        width: 200,
+        height: 50,
+        backgroundColor: COLORS.gray2,
+        borderRadius: SIZES.small / 2,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+    btnImg: (dimension) => ({
+        width: dimension,
+        height: dimension,
+        borderRadius: SIZES.small / 1.25,
+      }),
+});
+
+export default styles;
