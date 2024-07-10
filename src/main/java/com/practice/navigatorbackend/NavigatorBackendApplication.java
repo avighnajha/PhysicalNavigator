792e56cefbcb62ed00ccb04b8bb2bd40756cfbb5
Avighna Jha <avighnajha1703@gmail.com>
2024-07-10 15:58:52 +0100

    Introduced backend and reorganised structure.

---
+++ src/main/java/com/practice/navigatorbackend/NavigatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.practice.navigatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/navigatorbackend/NavigatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.navigatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
